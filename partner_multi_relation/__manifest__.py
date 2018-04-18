7b408e5c611d11fa46da5915d08bdd74cac3fd2e
David Dufresne <dufresne.david.2@gmail.com>
2018-04-18 12:06:35 -0400

    Enable additional columns on res_partner_relation_all.
    Enable to add additional columns from res_partner_relation in the sql view of res_partner_relation_all.

--- partner_multi_relation/__manifest__.py
+++ partner_multi_relation/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Partner Relations",
-    "version": "1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Therp BV,Camptocamp,Odoo Community Association (OCA)",
     "complexity": "normal",
     "category": "Customer Relationship Management",
